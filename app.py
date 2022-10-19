a06680b5d4456cd00c9ca20fa2fb5bd467523e5b
João Victor Magalhães Souza <joao.souza@rankmyapp.com.br>
2022-10-19 19:18:52 -0300

    Adicionado clausulas try e except

--- app.py
+++ app.py
@@ -16,51 +16,51 @@ st.write('''Esta aplicação tem como objetivo encontrar possíveis Keywords e s
  em textos.''')
 
 
-longa = st.text_area('Insira um texto:')
+longa = st.text_area('Insira um texto:', value="")
 
-########## Infos sobre o texto ##########
-wds = len(longa.split())
-chrt = len(longa)
-chrt_s_space = sum(len(x) for x in longa.split())
+if longa != "":
+	########## Infos sobre o texto ##########
+	wds = len(longa.split())
+	chrt = len(longa)
+	chrt_s_space = sum(len(x) for x in longa.split())
 
-idioma = st.selectbox(
-	'Idioma do texto:',
-	('Português','Inglês','Espanhol','Francês','Italiano','Alemão')
-)
+	idioma = st.selectbox(
+		'Idioma do texto:',
+		('Português','Inglês','Espanhol','Francês','Italiano','Alemão')
+	)
 
-values = [wds, chrt, chrt_s_space]
-index = ['Palavras','Caracteres','Caracteres s/ espaço']
+	values = [wds, chrt, chrt_s_space]
+	index = ['Palavras','Caracteres','Caracteres s/ espaço']
 
-info = pd.DataFrame(values, index=index, columns=['Informações sobre o Texto'])
-st.write(info)
-
-
-st.subheader('Keywords')
-st.write('Selecione o tipo de Keywords que deseja visualizar:')
-
-########## Head Tail ##########
-
-keywordfinder = kf.KeywordFinder(
-			long_description=longa,
-			strategy=all_strategies[idioma]
-)
-shortTail = keywordfinder.get_short_tail()
-headTail = keywordfinder.get_head_tail()
-longTail = keywordfinder.get_long_tail()
-
-# Visualizar Kwds
-if st.checkbox('Head Tail'):
-	st.write(headTail)
-	st.write('Densidade = Keyword / Número de Palavras')
+	info = pd.DataFrame(values, index=index, columns=['Informações sobre o Texto'])
+	st.write(info)
 
 
-# Visualizar Kwds
-if st.checkbox('Short Tail'):
-	st.write(shortTail)
-	st.write('Densidade = Keyword / Número de Palavras')
+	st.subheader('Keywords')
+	st.write('Selecione o tipo de Keywords que deseja visualizar:')
 
 
-# Visualizar Kwds
-if st.checkbox('Long Tail'):
-	st.write(longTail)
-	st.write('Densidade = Keyword / Número de Palavras')
+	keywordfinder = kf.KeywordFinder(
+				long_description=longa,
+				strategy=all_strategies[idioma]
+	)
+	shortTail = keywordfinder.get_short_tail()
+	headTail = keywordfinder.get_head_tail()
+	longTail = keywordfinder.get_long_tail()
+
+	# Visualizar Kwds
+	if st.checkbox('Head Tail'):
+		st.write(headTail)
+		st.write('Densidade = Keyword / Número de Palavras')
+
+
+	# Visualizar Kwds
+	if st.checkbox('Short Tail'):
+		st.write(shortTail)
+		st.write('Densidade = Keyword / Número de Palavras')
+
+
+	# Visualizar Kwds
+	if st.checkbox('Long Tail'):
+		st.write(longTail)
+		st.write('Densidade = Keyword / Número de Palavras')
--- utils/keyword_finder.py
+++ utils/keyword_finder.py
@@ -14,9 +14,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from utils import strategies as st
 
 
-'''
-    O Keyword Finder é único: ele 
-'''
+
 class KeywordFinder():
     def __init__(self, long_description: str, strategy: st.Strategy = st.Portuguese()):
         self.long_description = long_description
@@ -30,38 +28,49 @@ class KeywordFinder():
         
 
     def get_head_tail(self):
-        co = CountVectorizer(ngram_range=(1,1),stop_words=self.stopwords)
-        counts = co.fit_transform(pd.Series(self.long_description))
-        headTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False)
-        headTail.reset_index(inplace=True)
-        headTail.columns = ['Keyword', 'Count']
-        self.head_tail = headTail
-        return self.head_tail
+        try:
+            co = CountVectorizer(ngram_range=(1,1),stop_words=self.stopwords)
+            counts = co.fit_transform(pd.Series(self.long_description))
+            headTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False)
+            headTail.reset_index(inplace=True)
+            headTail.columns = ['Keyword', 'Count']
+            self.head_tail = headTail
+            return self.head_tail
+        
+        except:
+            return "Não foram encontradas head tails com o texto fornecido!"
 
     
     def get_short_tail(self):
-        co = CountVectorizer(ngram_range=(2,2))
-        counts = co.fit_transform(pd.Series(self.long_description))
-        shortTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False)
-        shortTail.reset_index(inplace=True)
-        shortTail.columns = ['Keyword', 'Count']
+        try:
+            co = CountVectorizer(ngram_range=(2,2))
+            counts = co.fit_transform(pd.Series(self.long_description))
+            shortTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False)
+            shortTail.reset_index(inplace=True)
+            shortTail.columns = ['Keyword', 'Count']
 
-        tmp_short = shortTail["Keyword"].str.split()
-        shortTail_clean = shortTail[~(tmp_short.str[0].isin(self.stopwords) | tmp_short.str[-1].isin(self.stopwords))]
+            tmp_short = shortTail["Keyword"].str.split()
+            shortTail_clean = shortTail[~(tmp_short.str[0].isin(self.stopwords) | tmp_short.str[-1].isin(self.stopwords))]
 
-        self.short_tail = shortTail_clean
-        return self.short_tail
+            self.short_tail = shortTail_clean
+            return self.short_tail
+        except:
+            return "Não foram encontradas short tails com o texto fornecido!"
     
     def get_long_tail(self):
-        co = CountVectorizer(ngram_range=(3,8))
-        counts = co.fit_transform(pd.Series(self.long_description))
-        longTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False).head()
-        longTail.reset_index(inplace=True)
-        longTail.columns = ['Keyword', 'Count']
+        try:
+            co = CountVectorizer(ngram_range=(3,8))
+            counts = co.fit_transform(pd.Series(self.long_description))
+            longTail = pd.DataFrame(counts.sum(axis=0),columns=co.get_feature_names()).T.sort_values(0,ascending=False).head()
+            longTail.reset_index(inplace=True)
+            longTail.columns = ['Keyword', 'Count']
 
-        tmp_long = longTail["Keyword"].str.split()
-        longTail_clean = longTail[~(tmp_long.str[0].isin(self.stopwords) | tmp_long.str[-1].isin(self.stopwords))]
+            tmp_long = longTail["Keyword"].str.split()
+            longTail_clean = longTail[~(tmp_long.str[0].isin(self.stopwords) | tmp_long.str[-1].isin(self.stopwords))]
 
-        self.long_tail = longTail_clean
-        return self.long_tail
+            self.long_tail = longTail_clean
+            return self.long_tail
+        except:
+            return "Não foram encontradas long tails com o texto fornecido!"
+
         
